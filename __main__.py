4b1cec2a29bd74ba4a0108505817a799def1388a
pome-ta <pome-ta@users.noreply.github.com>
2021-06-14 00:53:36 +0900

    すたーと

--- __main__.py
+++ __main__.py
@@ -5,16 +5,23 @@ import pdbg
 load_framework('ARKit')
 load_framework('SceneKit')
 
+ARSession = ObjCClass('ARSession')
 ARSCNView = ObjCClass('ARSCNView')
+ARFaceAnchor = ObjCClass('ARFaceAnchor')
 ARFaceTrackingConfiguration = ObjCClass('ARFaceTrackingConfiguration')
 
-pdbg.state(ARSCNView)
 
-
+configuration = ARFaceTrackingConfiguration.alloc()
+pdbg.state(configuration.maximumNumberOfTrackedFaces)
+#maximumNumberOfTrackedFaces
+class ViewController:
+  def __init__(self):
+    pass
 
 class View(ui.View):
   def __init__(self, *args, **kwargs):
     ui.View.__init__(self, *args, **kwargs)
+    self.instance = ObjCInstance(self)
 
 
 if __name__ == '__main__':
